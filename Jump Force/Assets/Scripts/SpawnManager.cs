936699fc90ecb40cf49cb9a0d2f8d778743fec7e
leespann <spannl@roverkids.org>
2024-05-24 13:35:04 -0400

    3.2 done

--- Jump Force/Assets/Scripts/SpawnManager.cs
+++ Jump Force/Assets/Scripts/SpawnManager.cs
@@ -8,11 +8,14 @@ public class SpawnManager : MonoBehaviour
     private Vector3 spawnPos = new Vector3(25, 0, 0);
     private float startDelay = 2;
     private float repeatRate = 2;
+    private PlayerController playerControllerScript;
 
     // Start is called before the first frame update
     void Start()
     {
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
         InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+        
     }
 
     // Update is called once per frame
@@ -23,7 +26,11 @@ public class SpawnManager : MonoBehaviour
 
     void SpawnObstacle()
     {
-        Instantiate(obsatclePrefab, spawnPos, obsatclePrefab.transform.rotation);
+        if(playerControllerScript.gameOver == false)
+        {
+            Instantiate(obsatclePrefab, spawnPos, obsatclePrefab.transform.rotation);
+        }
+       
     }
 
 }
